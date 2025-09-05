131a6432fbd089e33ffe514857ca0f43642781d6
Krystalcuarteros <charita121279@gmail.com>
2025-09-05 09:47:20 +0800

    Create Factorial.js
    Added

---
+++ Factorial.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+
+num1 = 5;
+num2 = 10;
+
+sum = num1 + num2;
+diff = num1 - num2;
+prod = num1 * num2;
+quot = num1 / num2;
+total = sum - diff;
+
+console.log("the sum of " + num1 + " and " + num2 + " is " + sum);
+console.log("the difference of " + num1 + " and " + num2 + " is " + diff);
+console.log("the production of " + num1 + " and " + num2 + " is " + prod);
+console.log("the quotient of " + num1 + " and " + num2 + " is " + quot);
+console.log("the total of " + sum + diff + " is " + total);
